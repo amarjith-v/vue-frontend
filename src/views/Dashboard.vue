af02a11c4f44e18df353ef8606aa1a213003119a
AMARJITH V <amarjith.v@bridge-global.com>
2022-11-16 21:48:18 +0530

    initial commit

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <Header :title="user"></Header>
+    <v-container>
+      <template>
+        <v-card v-for="order in orders" :key="order.id" class="mx-auto" max-width="344" outlined>
+          <v-list-item three-line>
+            <v-list-item-content>
+              <div class="text-overline mb-4">
+                {{order.status}}
+              </div>
+              <v-list-item-title class="text-h5 mb-1">
+                Headline 5
+              </v-list-item-title>
+              <v-list-item-subtitle>{{order.delivery_address}}</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+          </v-list-item>
+
+          <!-- <v-card-actions>
+            <v-btn outlined rounded text>
+              Button
+            </v-btn>
+          </v-card-actions> -->
+        </v-card>
+      </template>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+export default {
+  name: 'DashboardPage',
+  components: { Header },
+  data() {
+    return {
+      user: 'Amarjith v',
+      orders: []
+    }
+  },
+  created() {
+    this.getOrders();
+  },
+  methods: {
+    async getOrders() {
+      const user = JSON.parse(localStorage.getItem('userInfo'))
+      const config = {
+        headers: { Authorization: `Bearer ${user.data.token}` }
+      };
+      const url = 'http://127.0.0.1:8000/api/view/status';
+      await this.$http.get(url,config).then(res => {
+        this.orders = res.data.data.order
+        console.log(this.orders)
+      }).catch(err => {
+        if (err.response) {
+          console.log(err.response)
+        } else {
+          // TODO with toast
+          console.log("server error")
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <v-app id="inspire">
+      <v-main>
+        <v-container fluid fill-height>
+          <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md4>
+              <v-card class="elevation-12">
+                <v-toolbar dark color="primary">
+                  <v-toolbar-title>Login</v-toolbar-title>
+                </v-toolbar>
+                <v-card-text>
+                  <v-form ref="form">
+                    <v-text-field name="login" label="Username" :rules="emailRules" v-model="loginform.email"
+                      type="text"></v-text-field>
+                    <v-text-field id="password" name="password" :rules="passRules" v-model="loginform.password"
+                      label="Password" type="password">
+                    </v-text-field>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="primary" @click="submit" to="/">Login</v-btn>
+                    </v-card-actions>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-main>
+    </v-app>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'LoginPage',
+  data() {
+    return {
+      loginform: {
+        email: '',
+        password: ''
+      },
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+      passRules: [
+        v => !!v || 'Password is required',
+        v => v.length >= 8 || 'Password must be greater than 8 characters',
+      ],
+    }
+  },
+  methods: {
+    async submit() {
+      if (this.$refs.form.validate() == false) {
+        return
+      } else {
+        const url = 'http://127.0.0.1:8000/api/user/login';
+        await this.$http.post(url, this.loginform).then(res => {
+          localStorage.setItem("userInfo", JSON.stringify(res.data));
+          this.$router.push("/dashboard");
+        }).catch(err => {
+          if (err.response) {
+            if (err.response.status == 403) {
+              // TODO with toast
+              console.log("username or password is incorrect")
+            }
+          } else {
+            // TODO with toast
+            console.log("server error")
+          }
+        })
+      }
+    }
+  }
+}
+</script>
